773b77a982802a084d81037e0c7497f978d1f76e
如彼 <hua.qiuh@alibaba-inc.com>
2014-08-23 15:39:01 +0800

    setup workspace

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var gulp        = require('gulp');
+var sass        = require('gulp-ruby-sass');
+var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
+var jade        = require('gulp-jade');
+var sourcemaps  = require('gulp-sourcemaps');
+var coffee      = require('gulp-coffee');
+var gutil       = require('gulp-util');
+
+gulp.task('default', ['sass', 'jade', 'coffee', 'server'], function() {
+
+  gulp.watch('**/*.scss', ['sass', reload]);
+  gulp.watch('**/*.jade', ['jade', reload]);
+
+});
+
+/**
+ * @task jade
+ * compile jade template into html
+ */
+gulp.task('jade', function() {
+  return gulp.src('app/templates/*.jade')
+    .pipe(jade())
+    .pipe(gulp.dest('public/'));
+});
+
+/**
+ * @task sass
+ * compile sass into css
+ */
+gulp.task('sass', function() {
+  return gulp.src('app/stylesheets/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('public/stylesheets'));
+});
+
+/**
+ * @task coffee
+ * compile coffescript into javascript
+ */
+gulp.task('coffee', function() {
+  return gulp.src('app/scripts/*.coffee')
+    .pipe(sourcemaps.init())
+    .pipe(coffee({ bare: true })).on('error', gutil.log)
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('./public/scripts'));
+});
+
+/**
+ * @task server
+ * run a http server listening 3000 port
+ */
+gulp.task('server', function() {
+  browserSync({
+    server: {
+      baseDir: 'public'
+    }
+  });
+});
